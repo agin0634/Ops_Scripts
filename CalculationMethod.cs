2f5bc4dff3a29f0e5bee3907cdb2bd3aa7606a97
Duri Shen <agin0511@gmail.com>
2018-09-04 16:36:54 +0800

    init git

---
+++ CalculationMethod.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class CalculationMethod : MonoBehaviour {
+
+    public void Test()
+    {
+
+    }
+}
---
+++ TargetNumber.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetNumber : MonoBehaviour {
+
+    private CalculationMethod cm;
+    private int[] NumberPool = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    public int Difficulty = 4;
+    public List<int> Ref_Numbers;
+
+    void DrawBall()
+    {
+        for(int i = 1; i <= Difficulty; i++)
+        {
+            int x = Random.Range(0, NumberPool.Length-1);
+            int Number_Drawn = NumberPool[x];
+            Ref_Numbers.Add(Number_Drawn);
+            
+            Debug.Log(Ref_Numbers[i-1]);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        DrawBall();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
