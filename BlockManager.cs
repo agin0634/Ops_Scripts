19d84e893b62aedefb891f9280b223b94fe81755
Duri Shen <agin0511@gmail.com>
2018-09-07 18:16:51 +0800

    Add BlockManager , MainMenu scripts

---
+++ BlockManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockManager : MonoBehaviour {
+
+    public int Difficulty = 4;
+    public GameObject[] Block_Movable;
+
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i <= Difficulty - 1 ; i++)
+        {
+            Block_Movable[i].SetActive(true);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
