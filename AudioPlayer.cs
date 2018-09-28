a622a240b39ec025d6d6ed9af3ddac3141122ceb
Duri Shen <agin0511@gmail.com>
2018-09-28 21:34:19 +0800

    add Audio Player scripts

---
+++ AudioPlayer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour {
+
+    public AudioSource aS;
+    public int bIsToggleOn;
+
+	// Use this for initialization
+	void Start ()
+    {
+        aS = gameObject.GetComponent<AudioSource>();
+        
+        DontDestroyOnLoad(gameObject);
+        
+        bIsToggleOn = PlayerPrefs.GetInt("Settings_BGM");
+
+        if (bIsToggleOn == 0)
+        {
+            gameObject.GetComponent<AudioSource>().Pause();
+        }
+        else
+        {
+            gameObject.GetComponent<AudioSource>().Play();
+        }
+    }
+	
+	void Update ()
+    {
+		if(PlayerPrefs.GetInt("Settings_BGM") != bIsToggleOn)
+        {
+            bIsToggleOn = PlayerPrefs.GetInt("Settings_BGM");
+            SwitchToggle();
+        }
+	}
+
+    void SwitchToggle()
+    {
+        if (bIsToggleOn == 0)
+        {
+            gameObject.GetComponent<AudioSource>().Pause();
+            Debug.Log("Off");
+        }
+        else
+        {
+            gameObject.GetComponent<AudioSource>().Play();
+            Debug.Log("HO");
+        }
+    }
+}
