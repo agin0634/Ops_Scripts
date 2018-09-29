418b5b18d6a5b24be9bb4a23f8859987f932f8f0
Duri Shen <agin0511@gmail.com>
2018-09-29 20:37:46 +0800

    add Darkmode SFX settings scripts skeleton

---
+++ DarkModeToggle.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarkModeToggle : MonoBehaviour {
+
+    public int bIsToggleOn;
+    
+    void Start ()
+    {
+        
+        bIsToggleOn = PlayerPrefs.GetInt("Settings_DarkMode");
+
+        if (bIsToggleOn == 0)
+        {
+            // bright
+        }
+        else
+        {
+            // dark
+        }
+
+    }
+	
+	void Update ()
+    {
+        if (PlayerPrefs.GetInt("Settings_DarkMode") != bIsToggleOn)
+        {
+            bIsToggleOn = PlayerPrefs.GetInt("Settings_DarkMode");
+            SwitchToggle();
+        }
+    }
+
+    void SwitchToggle()
+    {
+        if (bIsToggleOn == 0)
+        {
+            //bright
+        }
+        else
+        {
+            //dark
+        }
+    }
+}
---
+++ SFXPlayer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXPlayer : MonoBehaviour {
+
+    public int bIsToggleOn;
+    
+    void Start ()
+    {
+
+        bIsToggleOn = PlayerPrefs.GetInt("Settings_SFX");
+
+        if (bIsToggleOn == 0)
+        {
+            // off
+        }
+        else
+        {
+            // on
+        }
+
+    }
+	
+	void Update ()
+    {
+        if (PlayerPrefs.GetInt("Settings_SFX") != bIsToggleOn)
+        {
+            bIsToggleOn = PlayerPrefs.GetInt("Settings_SFX");
+            SwitchToggle();
+        }
+
+    }
+
+    void SwitchToggle()
+    {
+        if (bIsToggleOn == 0)
+        {
+            // off
+        }
+        else
+        {
+            // on
+        }
+    }
+}
