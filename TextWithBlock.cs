3408dc93b4a5d9f1b706f92b07434bd258bfacc1
Duri Shen <agin0511@gmail.com>
2018-10-04 23:38:06 +0800

    fix number text show to fast bug

--- TextWithBlock.cs
+++ TextWithBlock.cs
@@ -4,16 +4,42 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class TextWithBlock : MonoBehaviour {
-
+    
     public Text NumberText;
     public int BlockNumber = 0;
+    public Animator Camera_Anim;
 
-	// Update is called once per frame
-	void FixedUpdate() {
+    private int Ori_Font_Size;
+    private bool bIsDone = false;
+
+    void Start()
+    {
+        if (!Camera_Anim)
+        {
+            Camera_Anim = FindObjectOfType<Camera>().GetComponent<Animator>();
+        }
+
+        NumberText.GetComponent<Text>().enabled = false;
+
+        Ori_Font_Size = NumberText.fontSize;
+        NumberText.fontSize = 0;
+
+    }
+    
+    
+	void FixedUpdate()
+    {
+        if(!bIsDone && Camera_Anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
+        {
+            NumberText.GetComponent<Text>().enabled = true;
+            bIsDone = true;
+        }
+
+
         Vector3 NumberPosition = Camera.main.WorldToScreenPoint(this.transform.position);
         NumberText.transform.position = NumberPosition;
-        
-        if(BlockNumber >= 0)
+
+        if (BlockNumber >= 0)
         {
             NumberText.text = BlockNumber.ToString();
         }
