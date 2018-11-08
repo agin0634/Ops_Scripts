b245f6b61e490c82348400c5764a9738199b78ff
Duri Shen <agin0511@gmail.com>
2018-11-08 20:21:20 +0800

    add Google Paly Services scripts

---
+++ PlayGameScripts.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SocialPlatforms;
+
+public class PlayGameScripts : MonoBehaviour {
+        
+	void Start ()
+    {
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
+           .Build();
+
+        PlayGamesPlatform.InitializeInstance(config);
+
+        PlayGamesPlatform.Activate();
+
+        SignIn();
+    }
+
+    void SignIn()
+    {
+        Social.localUser.Authenticate((bool success) => {
+            if (success)
+            {
+                ((GooglePlayGames.PlayGamesPlatform)Social.Active).SetGravityForPopups(Gravity.TOP);
+            }
+        });
+    }
+
+    #region Achievements
+
+    public static void ShowAchievementsUI()
+    {
+        Social.ShowAchievementsUI();
+    }
+
+    #endregion Achievements
+
+    #region Leaderboards
+
+    public static void ShowLeaderboardUI()
+    {
+        PlayGamesPlatform.Instance.ShowLeaderboardUI();
+    }
+
+    #endregion Leaderboards
+
+}
